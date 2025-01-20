3f96344616c5433e8c3964e6d9d0fb36cba934d1
Natalia Zinoveva <natalizin85@gmail.com>
2025-01-20 23:10:40 +1100

    Pеализация страницы main-empty.

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -16,7 +16,7 @@ function MainScreen({authorizationStatus, offers}: MainScreenProps): JSX.Element
   const dispatch = useAppDispatch();
 
   const mainCityClass: string = 'cities';
-  const isEmpty = offersByCity === undefined;
+  const isEmpty = offersByCity === undefined || offersByCity.length === 0;
 
   const handleClick = (city: string) => {
     if (currentCityName !== city) {
@@ -49,9 +49,17 @@ function MainScreen({authorizationStatus, offers}: MainScreenProps): JSX.Element
       </div>
       <div className={mainCityClass}>
         {!isEmpty ?
-          (
-            <MemoizedOffersList currentCity={offersByCity[0].city} citiesClassName={mainCityClass} authorizationStatus={authorizationStatus} />
-          ) : <p>There is no current offers for this city</p>}
+          <MemoizedOffersList currentCity={offersByCity[0].city} citiesClassName={mainCityClass} authorizationStatus={authorizationStatus} />
+          :
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">We could not find any property available at the moment in {currentCityName}</p>
+              </div>
+            </section>
+            <div className="cities__right-section"></div>
+          </div>}
       </div>
     </main>
   );
